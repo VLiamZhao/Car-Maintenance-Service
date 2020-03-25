fda04c8f383d1cabdd6ed45f8642c3807d223e27
Can Zhao <canzhao216@gmail.com>
2020-03-25 13:07:28 -0400

    controller1

--- src/main/java/org/mynode/controller/CustomerController.java
+++ src/main/java/org/mynode/controller/CustomerController.java
@@ -4,9 +4,7 @@ import org.mynode.model.Customer;
 import org.mynode.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.*;
 import java.util.List;
@@ -17,9 +15,37 @@ public class CustomerController {
     @Autowired
     CustomerService customerService;
 
-    //{prefix}/customer     GET
+    /**
+     * GET {prefix}/customer
+     * @param
+     * @return
+     */
     @RequestMapping(value = "", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
     public List<Customer> getCustomers(){
         return customerService.getCustomers();
     }
+
+    /**
+     * POST {prefix}/customer/body
+     * @param customer
+     * @return
+     */
+    @RequestMapping(value = "/body", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public Customer createCustomer(@RequestBody Customer customer){
+        Customer cu = customerService.save(customer);
+        return cu;
+    }
+
+    /**
+     * GET {prefix}/customer/{id}
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = {MediaType.APPLICATION_JSON_VALUE})
+    public Customer getCustomerById(@PathVariable long id){
+        Customer customer = customerService.getCustomerById(id);
+        return customer;
+    }
+
+    
 }
