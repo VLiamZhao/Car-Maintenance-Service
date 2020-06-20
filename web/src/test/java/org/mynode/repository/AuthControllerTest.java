b477a76ac09e24e869671e4298e42d8cdae13be2
Can Zhao <canzhao216@gmail.com>
2020-06-20 05:06:31 -0400

    update readme2

--- web/src/test/java/org/mynode/repository/AuthControllerTest.java
+++ web/src/test/java/org/mynode/repository/AuthControllerTest.java
@@ -1,56 +1,56 @@
-//package org.mynode.repository;
-//
-//import org.junit.Assert;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mynode.init.ApplicationBootstrap;
-//import org.mynode.model.Customer;
-//import org.mynode.model.Role;
-//import org.mynode.service.CustomerService;
-//import org.mynode.service.JWTService;
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import java.util.List;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = ApplicationBootstrap.class)
-//public class AuthControllerTest {
-//    private Logger logger = LoggerFactory.getLogger(getClass());
-//    String email = "joe@gmail.com";
-//    String pw = "202cb962ac59075b964b07152d234b70";
-//
-//    @Autowired
-//    CustomerService customerService;
-//    @Autowired
-//    JWTService jwtService;
-//
-////    @Before
-////    public void init(){
-////        c = new Customer("ab");
-////        c.setEmail(email);
-////        c.setPassword(pw);
-////        c = customerService.save(c);
-////        Assert.assertNotNull(c);
-////    }
-////
-////    @After
-////    public void tearDown(){
-////        Assert.assertEquals(customerService.deleteCustomerById(c.getId()), true);
-////    }
-//
-//    @Test
-//    public void getCustomerByCredentials() throws Exception {
-//        Customer c1 = customerService.getCustomerByCredentials(email, pw);
-//        List<Role> l = c1.getRoleList();
-//        for(Role r : l){
-//
-//        }
-//        Assert.assertNotNull(c1);
+package org.mynode.repository;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mynode.init.ApplicationBootstrap;
+import org.mynode.model.Customer;
+import org.mynode.model.Role;
+import org.mynode.service.CustomerService;
+import org.mynode.service.JWTService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApplicationBootstrap.class)
+public class AuthControllerTest {
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    String email = "joe@gmail.com";
+    String pw = "202cb962ac59075b964b07152d234b70";
+
+    @Autowired
+    CustomerService customerService;
+    @Autowired
+    JWTService jwtService;
+
+//    @Before
+//    public void init(){
+//        c = new Customer("ab");
+//        c.setEmail(email);
+//        c.setPassword(pw);
+//        c = customerService.save(c);
+//        Assert.assertNotNull(c);
 //    }
 //
-//
-//}
+//    @After
+//    public void tearDown(){
+//        Assert.assertEquals(customerService.deleteCustomerById(c.getId()), true);
+//    }
+
+    @Test
+    public void getCustomerByCredentials() throws Exception {
+        Customer c1 = customerService.getCustomerByCredentials(email, pw);
+        List<Role> l = c1.getRoleList();
+        for(Role r : l){
+
+        }
+        Assert.assertNotNull(c1);
+    }
+
+
+}
--- web/src/test/java/org/mynode/service/MessageServiceTest.java
+++ web/src/test/java/org/mynode/service/MessageServiceTest.java
@@ -1,31 +1,31 @@
-//package org.mynode.service;
-//
-//import com.amazonaws.services.sqs.AmazonSQS;
-//import com.amazonaws.services.sqs.model.GetQueueUrlResult;
-//import com.amazonaws.services.sqs.model.SendMessageRequest;
-//import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.mynode.init.ApplicationBootstrap;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
-//
-//import static org.mockito.ArgumentMatchers.anyString;
-//import static org.mockito.Mockito.*;
-//
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(classes = ApplicationBootstrap.class)
-//public class MessageServiceTest {
-//    @Autowired
-//    private MessageService messageService;
-//    @Autowired
-//    private AmazonSQS amazonSQS;
-//
-//
-//    @Test
-//    public void sendMessageTest(){
-//        when(amazonSQS.getQueueUrl(anyString())).thenReturn(mock(GetQueueUrlResult.class));
-//        messageService.sendMessage("hello",1);
-//        verify(amazonSQS,times(1)).sendMessage(any(SendMessageRequest.class));
-//    }
-//}
+package org.mynode.service;
+
+import com.amazonaws.services.sqs.AmazonSQS;
+import com.amazonaws.services.sqs.model.GetQueueUrlResult;
+import com.amazonaws.services.sqs.model.SendMessageRequest;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mynode.init.ApplicationBootstrap;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApplicationBootstrap.class)
+public class MessageServiceTest {
+    @Autowired
+    private MessageService messageService;
+    @Autowired
+    private AmazonSQS amazonSQS;
+
+
+    @Test
+    public void sendMessageTest(){
+        when(amazonSQS.getQueueUrl(anyString())).thenReturn(mock(GetQueueUrlResult.class));
+        messageService.sendMessage("hello",1);
+        verify(amazonSQS,times(1)).sendMessage(any(SendMessageRequest.class));
+    }
+}
