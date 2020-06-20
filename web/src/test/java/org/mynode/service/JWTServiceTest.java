33355506a68cb7df7723b3cfb898794d90b47258
Can Zhao <canzhao216@gmail.com>
2020-06-20 05:02:50 -0400

    update readme

--- web/src/test/java/org/mynode/service/JWTServiceTest.java
+++ web/src/test/java/org/mynode/service/JWTServiceTest.java
@@ -12,40 +12,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ApplicationBootstrap.class)
 public class JWTServiceTest {
-    @Autowired
-    JWTService jwtService;
-    private Logger logger = LoggerFactory.getLogger(getClass());
-    //passed
-//    @Test
-//    public void generateTokenTest(){
-//        Customer customer = new Customer();
-//        customer.setId(100L);
-//        customer.setName("CZ");
-//        String token = jwtService.generateToken(customer);
-//    //        Assert.assertNotNull(token);
-//    // TODO use regular expression - done
-//    // String[] arr = token.split("\\.");
-//
-//        String patternString = "^[A-Za-z0-9_]+\\.[A-Za-z0-9_]+\\.?[A-Za-z0-9_]*$";
-//        Pattern pattern = Pattern.compile(patternString);
-//        Matcher matcher = pattern.matcher(token);
-//        Assert.assertTrue(token, matcher.matches());
-//    }
-    //passed
-    @Test
-    public void decodeTokenTest(){
-        Customer customer = new Customer();
-        customer.setId(100L);
-        customer.setName("CZ");
-        String token = jwtService.generateToken(customer);
-        Claims c = jwtService.decodeJwtToken(token);
-        String username = c.getSubject();
-        Assert.assertEquals(customer.getName(), username);
-    }
+  @Autowired JWTService jwtService;
+  private Logger logger = LoggerFactory.getLogger(getClass());
+  // passed
+  //    @Test
+  //    public void generateTokenTest(){
+  //        Customer customer = new Customer();
+  //        customer.setId(100L);
+  //        customer.setName("CZ");
+  //        String token = jwtService.generateToken(customer);
+  //    //        Assert.assertNotNull(token);
+  //    // TODO use regular expression - done
+  //    // String[] arr = token.split("\\.");
+  //
+  //        String patternString = "^[A-Za-z0-9_]+\\.[A-Za-z0-9_]+\\.?[A-Za-z0-9_]*$";
+  //        Pattern pattern = Pattern.compile(patternString);
+  //        Matcher matcher = pattern.matcher(token);
+  //        Assert.assertTrue(token, matcher.matches());
+  //    }
+  // passed
+  @Test
+  public void decodeTokenTest() {
+    Customer customer = new Customer();
+    customer.setId(100L);
+    customer.setName("CZ");
+    String token = jwtService.generateToken(customer);
+    Claims c = jwtService.decodeJwtToken(token);
+    String username = c.getSubject();
+    Assert.assertEquals(customer.getName(), username);
+  }
 }
